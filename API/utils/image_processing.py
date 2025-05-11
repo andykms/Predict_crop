ef08d36849d2d0484342a4c8536c62f465d526f3
Andrey Isaev <andrey.isaev.54.ru@gmail.com>
2025-05-11 03:25:35 +0300

    refactor: resize image before send to UNET

--- API/utils/image_processing.py
+++ API/utils/image_processing.py
@@ -57,7 +57,7 @@ def predict_mask(img_normalized):
 
 def predict_points(img):
     #Изменяем размер изображения
-    img = resize_image(img, (100, 100))
+    img = resize_image(img, (240, 240))
     img_colored = copy.deepcopy(img);
     print(img_colored)
     if img.shape[-1] == 4:
